79dc5ea9eff24c14eca8e5ad697fd0a6e512f9e8
Hlib <u140_hlishu_waw@technischools.com>
2024-06-06 14:14:45 +0200

    random e

--- generate_keys.py
+++ generate_keys.py
@@ -1,15 +1,16 @@
 from sympy import randprime
 from math import gcd
+from random import choice
 
 def keys_generator():
     p, q = randprime(100, 999), randprime(100, 999)
     n = p*q
     fn = (p-1)*(q-1)
-    e = 0
-    for i in range(2, fn):
+    e = []
+    for i in range(fn, 2, -1):
         if gcd(i, fn) == 1:
-            e = i 
-            break
+            e.append(i)
+    e = choice(e)
     d = pow(e, -1, fn)
     return [(n, e), (n, d)]
            
