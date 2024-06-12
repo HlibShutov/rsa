9cc634bde5891a28c99aca093f5068df6a324f8e
Hlib <u140_hlishu_waw@technischools.com>
2024-06-12 14:27:20 +0200

    key exchange

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
 import socket
+from generate_keys import keys_generator
 
 HOST, PORT = '127.0.0.1', 1488
+keys = keys_generator()
+public_key, private_key = keys[0], keys[1]
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
@@ -8,3 +12,10 @@ try:
     print('connected to server')
 except:
     print('error while connecting to server')
+
+while True:
+    message = client.recv(124)
+    public_server_key = message.decode()
+    print(public_server_key)
+    client.send(bytes(' '.join(list(map(str, public_key))), 'utf-8'))
+    if message: break
--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 import socket
+from generate_keys import keys_generator
 
 HOST, PORT = '127.0.0.1', 1488
+
+keys = keys_generator()
+public_key, private_key = keys[0], keys[1]
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -15,4 +19,7 @@ server.listen(5)
 while True:
     client, address = server.accept()
     print(f"user connected on address: {address[0]}:{address[1]}")
-    if client: break
+    client.send(bytes(' '.join(list(map(str,public_key))), 'utf-8'))
+    client_public_key = client.recv(124).decode()
+    print(client_public_key)
+    break
